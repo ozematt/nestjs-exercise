c39accb031abd64c6df0f11695a6007ea9ceaa4a
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-04 12:34:29 +0100

    added config test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,9 +4,11 @@ import {
   INestApplication,
   ValidationPipe,
 } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef =
       await Test.createTestingModule({
@@ -21,10 +23,11 @@ describe('App e2e', () => {
     );
 
     await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDB();
   });
   afterAll(() => {
     app.close();
   });
-
-  it.todo('should work');
 });
