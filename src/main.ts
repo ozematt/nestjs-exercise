3143b235b94c12ac6c1d10d21d3394776b76cfb0
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-05 18:54:57 +0100

    edited config

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
